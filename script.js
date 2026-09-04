ec4789f5a34f47e9b84e33eb758b6929d81501ee
daviddchucks-hash <daviddchucks@gmail.com>
2026-09-04 09:49:37 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-```javascript
+
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
+ const display = document.getElementById("display");
     const message = document.getElementById('message').value;
+
+  const prompt = document.getElementById("promptbtn");
 
     if (name && email && message) {
         alert('Thank you for your message, ' + name + '! We will get back to you soon.');
@@ -12,3 +15,16 @@ document.getElementById('contactForm').addEventListener('submit', function(event
         alert('Please fill in all fields.');
     }
 });
+
+prompt.addEventListener("click", () => {
+
+   let amount = prompt("How much do u want to save");
+  if (amount === "") {
+    alert("pls input amount");
+  }
+  else {
+document.getElementById("amountadded").textContent = "Amount Added successfully";
+  }
+display.textContent = amount
+  
+})
